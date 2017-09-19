bec2953029ea6cf0b45055507457d748f151d7c5
Michal Gregor <michal@gregor.sk>
2017-09-19 13:52:23 +0200

    No need for the make_pdf script; instructions are part of readme now.

--- make_pdf.sh
+++
@@ -1,3 +0,0 @@
-mkdir -p auxfiles
-latexmk -silent -bibtex -outdir=auxfiles -pdf -pdflatex="pdflatex -synctex=1 --shell-escape %O '\def\auxfiles{}\input{%S}'" main.tex
-# Command for TexLive: latexmk -silent -bibtex -outdir=auxfiles -pdf -pdflatex="pdflatex -synctex=1 --shell-escape %%O '\def\auxfiles{}\input{%%S}'" %
